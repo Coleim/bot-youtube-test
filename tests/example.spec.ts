6072783d886823d24a6b3d6a0675d23549874a5e
Clément Oliva <clement.oliva@gmail.com>
2024-04-29 22:27:09 +0200

    add debugs

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 
 
-const nbVideo = 100
+const nbVideo = 10
 // for( let i = 0; i < nbVideo; i++ ) {
 //   test(`View Youtube Non Repertoriee ${i}`, async ({ page }) => {
 //     test.setTimeout(120000)
@@ -15,11 +15,14 @@ const nbVideo = 100
 
 for( let i = 0; i < nbVideo; i++ ) {
   test(`View Youtube Publique ${i}`, async ({ page }) => {
+    console.log("Opening video", i)
     await page.goto('https://www.youtube.com/watch?v=G5KZoBZA1-I');
-  
+    console.log("Accepting consent", i)
     await page.locator('button:has-text("Accept All")').click();
   
     //random number between 50000 and 120000
-    await page.waitForTimeout(Math.floor(Math.random() * 80000) + 50000);
+    const randomTime = Math.floor(Math.random() * 80000) + 50000
+    console.log("Waiting for random time", i, randomTime)
+    await page.waitForTimeout(randomTime);
   });
 }
